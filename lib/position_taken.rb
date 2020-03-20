94aa190c67ec4b0a1d9ecbaa437f41634f64df22
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-03-20 21:02:22 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,4 @@
 def position_taken?(board, index)
-  taken = nil;
-  if (board[index] == " " || board[index] == "" || board[index] == nil) 
-    taken = false 
-    else 
-      taken = true
-end
-
-taken
+! (board[index] ==  " " || board[index] == "" || board[index] == nil) ?
+  false : true
+  
